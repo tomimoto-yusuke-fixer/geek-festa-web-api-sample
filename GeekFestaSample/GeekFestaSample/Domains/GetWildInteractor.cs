231f0afdd3d99ca3a890a7706d5df03cb276c27b
tomimoto <tomimoto.yusuke@fixer.co.jp>
2021-03-11 15:42:57 +0900

    add cpu load

--- GeekFestaSample/GeekFestaSample/Domains/GetWildInteractor.cs
+++ GeekFestaSample/GeekFestaSample/Domains/GetWildInteractor.cs
@@ -1,14 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using GeekFestaSample.UseCases;
 
 namespace GeekFestaSample.Domains
 {
     public class GetWildInteractor : IGetWildUseCase
     {
-        public GetWildInteractor(){}
-    
-        public string Get() 
+        public GetWildInteractor()
         {
+        }
+
+        public string Get()
+        {
+            Task.Delay(5000).ContinueWith((task) => Num());
             return "Wild";
+        }
+
+        private void Num()
+        {
+            long num = 0;
+            var random = new Random();
+            while (num < 10000000000)
+            {
+                num += random.Next(100, 1000);
+            }
         }
     }
 }
